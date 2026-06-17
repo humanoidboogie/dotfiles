3e3f9353e8d1b63abb5ca43ba79e36c334316960
Colin Wilson <furious.gecko@gmail.com>
2026-06-17 15:36:01 +0100

    removed 'worksession' command

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,10 +4,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias ls='ls -l --color=auto'
     alias grep='grep --color=auto'
-
-    function worksession
-        timer $argv && notify-send -a Timer -u critical "Time's Up!" -i hourglass
-    end
 end
 
 fish_add_path ~/.local/bin
