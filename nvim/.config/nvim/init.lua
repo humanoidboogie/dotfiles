32f88276ec26d4da88480eea70c451a5bf716495
Colin Wilson <furious.gecko@gmail.com>
2026-09-11 13:51:27 +0100

    removed a load of lazyvim plugins I don't use any more

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("catppuccin").load("mocha")
-
-require("section-wordcount").setup({
-    -- These are the default values and can be omitted
-    highlight = "String",
-    virt_text_pos = "eol",
-})
--- nvim/.config/nvim/lua/plugins/section-wordcount.lua
+++
@@ -1,3 +0,0 @@
-return {
-    "humanoidboogie/section-wordcount.nvim",
-}
